6c76cc4ae726e79f4189a365d667180a0c6acbe4
tsots <1katalist@gmail.com>
2021-04-30 05:48:31 +0200

    first commit initializing setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../styles/Global";
+import { theme } from "../styles/Theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
