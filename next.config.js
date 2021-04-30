59ab9cd2079f37556d193bb5f4854676ad5f3feb
tsots <1katalist@gmail.com>
2021-04-30 07:00:28 +0200

    push from vscode

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withImages = require("next-images");
+
+module.exports = withImages();
+// module.exports = {
+//     images: {
+//         loader: "imgix",
+//         path: "/",
+//     },
+// };
