83b455b84948a68c5df48e8b72e81357cf1b60b4
git-katalist <79943903+git-katalist@users.noreply.github.com>
2021-04-30 07:50:38 +0000

    Deploying to gh-pages from  @ 11171e7276950598a8c0716b7aa1c6aba32d0b0c 🚀

---
+++ _next/static/pS3HITJz3VMtUnK8Dzk_e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-c7189eb51f45c7ad5dc6.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3625d0b0e5533e21b970.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
